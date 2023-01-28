e6f80e2f79fc0aae2fd75fc078c8b4599ca7fd6b
theoneinyourdream <73285363+theoneinyourdream@users.noreply.github.com>
2023-01-28 18:08:37 +0530

    first commit

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import {router} from './router'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import "./index.css"
+import { RouterProvider } from "react-router-dom"
+import { router } from "./router"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import "stream-chat-react/dist/css/index.css"
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const queryClient = new QueryClient()
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  </React.StrictMode>
 )
